54827a2caf22e6761dc7fdccba1847cf6558ae34
kate <01redcat@mail.ru>
2022-07-11 01:01:18 +0300

    not yet Task8

--- Task8/Program.cs
+++ Task8/Program.cs
@@ -1,4 +1,4 @@
-﻿// Задача 8: Напишите программу, 
+﻿ // Задача 8: Напишите программу, 
 //которая на вход принимает число (N), 
 //а на выходе показывает все чётные числа от 1 до N.
 //5 -> 2, 4
@@ -6,11 +6,11 @@
 Console.WriteLine("Введите целое число ");
 int n = Convert.ToInt32(Console.ReadLine());
 int count = 1;
-while(count<=n) 
-   if(count%2==0) 
-     {
-      Console.Write(count);
-     }
-   else count++;
-count ++;  
-
+while (count<=n)
+{
+  if(count%2==0) 
+  {
+   Console.Write(count);
+  } 
+ count ++;
+}
