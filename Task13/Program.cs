20459b02e0e4a987edbc4e9ff4c37459b98a45d8
kate <01redcat@mail.ru>
2022-07-16 15:58:59 +0300

    Task 13 fixed

--- Task13/Program.cs
+++ Task13/Program.cs
@@ -2,7 +2,7 @@
 //645 -> 5
 //78 -> третьей цифры нет
 //32679 -> 6
-Console.WriteLine("Введите число ");
+Console.Write("Введите число ");
 int number = Convert.ToInt32(Console.ReadLine());
 int ThirthDigit(int num)
 {
@@ -10,12 +10,15 @@ int ThirthDigit(int num)
     {
         return -1;
     }  
-    while (number > 1000) number = number / 10;
+    while (number > 1000) 
+    {
+        number = number / 10;
+    }
     int number1 = number;
     int find = number1 % 10;
     return find;
 }
 ThirthDigit(number);
 int digit3 = ThirthDigit(number);
-if(digit3 == -1) Console.WriteLine($"{number} -> третьей цифры нет");
-else Console.WriteLine($"{number} -> {digit3}");
+if(digit3 == -1) Console.Write($"{number} -> третьей цифры нет");
+else Console.Write($" -> {digit3}");
