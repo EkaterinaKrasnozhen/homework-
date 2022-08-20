4283ff1511595f19a7fa50f6d34e008af3e31abb
kate <01redcat@mail.ru>
2022-08-20 11:49:35 +0300

    Task 60 3 мерный массив создание и печать, с 3мя индексами

--- Task58/Program.cs
+++ Task58/Program.cs
@@ -1,33 +1,19 @@
 ﻿/* Задача 58: Задайте две матрицы. 
 Напишите программу, которая будет находить произведение двух матриц.
 Например, заданы 2 массива:
-
 1 4 7 2
-
 5 9 2 3
-
 8 4 2 4
-
 5 2 6 7
-
 и
-
 1 5 8 5
-
 4 9 4 2
-
 7 2 2 6
-
 2 3 4 7
-
 Их произведение будет равно следующему массиву:
-
 1 20 56 10
-
 20 81 8 6
-
 56 8 4 24
-
 10 6 24 49 */
 int[,] GetMatrix(int m, int n)
 {
@@ -60,14 +46,14 @@ void PrintMatrix(int[,] matr)
 PrintMatrix(matrix1);
 Console.WriteLine();
 PrintMatrix(matrix2);
-int[,] MultiMatrix(int[,] matr1, int [,] matr2)
+int[,] MultiMatrix(int[,] matr1, int[,] matr2)
 {
     int[,] multimatr = new int[matr1.GetLength(0), matr1.GetLength(1)];
     for (int i = 0; i < multimatr.GetLength(0); i++)
     {
         for (int j = 0; j < multimatr.GetLength(1); j++)
         {
-            multimatr[i, j] = matr1[i, j]*matr2[i,j];
+            multimatr[i, j] = matr1[i, j] * matr2[i, j];
         }
     }
     return multimatr;
---
+++ Task60/Program.cs
@@ -0,0 +1,42 @@
+﻿/*Задача 60. Сформируйте трёхмерный массив из неповторяющихся двузначных чисел. 
+Напишите программу, которая будет построчно выводить массив, 
+добавляя индексы каждого элемента.
+массив размером 2 x 2 x 2
+
+12(0,0,0) 22(0,0,1)
+45(1,0,0) 53(1,0,1) */
+int[,,] GetMatrix(int m, int n, int z)
+{
+    int[,,] matrix = new int[m, n, z];
+    Random rnd = new Random();
+    for ( int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for ( int j = 0; j < matrix.GetLength(1); j++)
+        {
+            for ( int k = 0; k < matrix.GetLength(2); k++)
+            {
+                matrix[i, j, k] = rnd.Next(1, 45);
+            }
+            
+        }
+    }
+    return matrix;
+}
+int [,,] matr3 = GetMatrix(2,2,2);
+void PrintMatrix(int[,,] matr)
+{
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            for (int z = 0; z < matr.GetLength(2); z++)
+            {
+            
+            if (z < matr.GetLength(1)) Console.Write($"{matr[i, j, z],3} ({i},{j},{z}) ");
+            
+            }
+        }
+        Console.WriteLine();
+    }
+}
+PrintMatrix(matr3);
